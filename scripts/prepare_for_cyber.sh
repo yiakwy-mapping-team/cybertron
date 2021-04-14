be4fa872d2b28e0906f95402521354ea2e64e069
yiakwy-mapping-team <yi.wang@plus.ai>
2021-04-14 11:33:29 -0400

    add(scripts): add cybertron installer

---
+++ scripts/prepare_for_cyber.sh
@@ -0,0 +1,7 @@
+sudo bash installers/install_minimal_environment.sh cn
+sudo bash installers/install_cyber_deps.sh download
+sudo bash installers/install_llvm_clang.sh
+sudo bash installers/install_qa_tools.sh
+sudo bash installers/install_visualizer_deps.sh
+sudo bash installers/install_bazel.sh
+sudo bash installers/post_install.sh cyber
